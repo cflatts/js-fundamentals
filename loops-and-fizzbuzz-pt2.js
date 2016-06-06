11068a224a9bbebb5d39516518ef33d4214ae5ce
Caitlin Flattery <caitlinflattery@Caitlins-Air.attlocal.net>
2016-06-06 07:02:05 -0500

    loops pt 2 part 5ish

--- loops-and-fizzbuzz-pt2.js
+++ loops-and-fizzbuzz-pt2.js
@@ -1,16 +1,16 @@
 // PART 0: Write a function called squareDance() that squares each number in an array.
 //
 
-// var squareDance = function(number) {
-//     for(var i = 0; i < number.length; i++) {
-//         var squaredNumber = number[i]*number[i]
-//         }return squaredNumber
-// }
+var squareDance = function(number) {
+    for(var i = 0; i < number.length; i++) {
+        var squaredNumber = number[i]*number[i]
+        }return squaredNumber
+}
 
 
-// console.assert(squareDance([1, 2])[1] === 4)
-// console.assert(squareDance([5,10,15])[2] === 225)
-// console.assert(squareDance([3,6,9,3])[0] === 9)
+console.assert(squareDance([1, 2])[1] === 4)
+console.assert(squareDance([5,10,15])[2] === 225)
+console.assert(squareDance([3,6,9,3])[0] === 9)
 
 
 
@@ -32,18 +32,18 @@ console.assert(nicer("mom get the heck in here and bring me a darn sandwich.") =
 
 console.assert(nicer("here son, your crappy sandwich is on the dang plate.") === "here son, your sandwich is on the plate.")
 
-// PART 2: write a function called capitalizeAll(). It should take as input a sentence and capitalize the first letter of every word in the sentence.
+PART 2: write a function called capitalizeAll(). It should take as input a sentence and capitalize the first letter of every word in the sentence.
 
-// var capitalizeAll = function(string) {
-//     var splitString = string.split(' ')
-//     for(var i = 0; i < splitString.length; i++) {
-//         splitString[i] = splitString[i].charAt(0).toUpperCase() + splitString[i].substring(1)
-//         var allCaps = splitString.join(' ')
-//         } return allCaps
-// }
+var capitalizeAll = function(string) {
+    var splitString = string.split(' ')
+    for(var i = 0; i < splitString.length; i++) {
+        splitString[i] = splitString[i].charAt(0).toUpperCase() + splitString[i].substring(1)
+        var allCaps = splitString.join(' ')
+        } return allCaps
+}
 
 
-// log(capitalizeAll('every day is like sunday.') === 'Every Day Is Like Sunday.')
+log(capitalizeAll('every day is like sunday.') === 'Every Day Is Like Sunday.')
 
 
 
@@ -68,14 +68,14 @@ console.assert(properSentences(paragraph) === "It was a fine morning. The wine w
 
 // PART 4: write a function called iPutTheFunIn(). It should take a string as input. The output should be a copy of the original string with the word 'fun' inserted into the center of the string.
 
-// var iPutTheFunIn = function (input) {
-//     var funWord = 'fun'
-//     var fullFunWord = input.substr(0, (input.length / 2)) + funWord + input.substr((input.length / 2), input.length)
-//     return fullFunWord
-// }
+var iPutTheFunIn = function (input) {
+    var funWord = 'fun'
+    var fullFunWord = input.substr(0, (input.length / 2)) + funWord + input.substr((input.length / 2), input.length)
+    return fullFunWord
+}
 
-// console.assert(iPutTheFunIn("funerary") === "funefunrary")
-// console.assert(iPutTheFunIn("reds") === "refunds")
+console.assert(iPutTheFunIn("funerary") === "funefunrary")
+console.assert(iPutTheFunIn("reds") === "refunds")
 
 // PART 5: write a function called pipeline(). it should take three inputs: (1) a starting value, (2) a function, and (3) another function. it should use functions (2) and (3) on the starting value, one after the other, and return a new value that has been processed by both function (2) and function (3).
 
@@ -83,6 +83,12 @@ console.assert(properSentences(paragraph) === "It was a fine morning. The wine w
 
 // test 1
 var paragraph = 'mom bring your crappy self in here. i want a dang sandwich.'
+
+var pipeline = function (inputValue, inputFunc1, inputFunc2) {
+    var cleanSentence = inputFunc1(inputValue)
+    var allProper = inputFunc2(cleanSentence)
+    return allProper
+}
 
 console.assert(pipeline(paragraph,nicer,properSentences) === "Mom bring your self in here. I want a sandwich.")
 
@@ -95,6 +101,13 @@ var addOne = function(n) {
 	return n + 1
 }
 
+var pipeline = function (inputValue, inputFunc1, inputFunc2) {
+    var squared = inputFunc1(inputValue)
+    var squaredPlusOne = inputFunc2(squared)
+    return squaredPlusOne
+
+}
+
 console.assert(pipeline(7,squareNum,addOne) === 50)
 
 // test 3
@@ -103,6 +116,12 @@ var exclaimAll = function(arr) {
 	for (var i = 0; i < arr.length; i ++) {
 		newArr.push(arr[i] + '!')
 	}
+}
+
+var pipeline = function (inputValue, inputFunc1, inputFunc2) {
+    var squared = inputFunc1(inputValue)
+    var exclaSquared = inputFunc2(squared)
+    return exclaSquared
 }
 
 var result = pipeline([10,20,30],squareDance,exclaimAll)
