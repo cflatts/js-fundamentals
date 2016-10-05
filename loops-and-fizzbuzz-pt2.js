814703e4d244564b831ecb06578490c7f5b62c61
Caitlin Flattery <caitlinflattery@gmail.com>
2016-10-05 18:03:12 -0500

    pt 2 done

--- loops-and-fizzbuzz-pt2.js
+++ loops-and-fizzbuzz-pt2.js
@@ -118,6 +118,7 @@ var exclaimAll = function(arr) {
 	for (var i = 0; i < arr.length; i ++) {
 		newArr.push(arr[i] + '!')
 	}
+    return newArr
 }
 
 var pipeline = function (inputValue, inputFunc1, inputFunc2) {
